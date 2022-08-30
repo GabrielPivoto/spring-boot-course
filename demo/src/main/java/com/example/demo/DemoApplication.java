418f30f69fea9cc6853aa58784ab860c817afdc7
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-08-30 11:05:23 -0300

    Retornando array de estudantes

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.model.Estudante;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,7 +18,12 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Estudante> hello(){
+		return List.of(new Estudante(
+				"Gabriel",
+				22,
+				LocalDate.of(1999,8,31),
+				"gabriel-pivoto@outlook.com"
+		));
 	}
 }
